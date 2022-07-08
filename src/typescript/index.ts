eb62c9cd8f7f221588d042aa19f94e2b7571332d
Salman H <Salman0112358@outlook.com>
2022-07-08 17:37:44 +0100

    fixing index.ts to incorperate fixes for typescript runtime

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,17 +1,17 @@
-let numberOne = 5;
-const numberTwo = 10;
-numberOne = "7";
+let numberOne : number = 5;
+const numberTwo : number = 10;
+numberOne = 7;
 
-const additionResult = numberOne + numberTwo;
+const additionResult : number = numberOne + numberTwo;
 console.log(`${numberOne} plus ${numberTwo} is... ${additionResult}`);
 
-const arrOne = [1, 2, 3];
-const arrTwo = [4, 5, 6];
-const joinedArr = arrOne + arrTwo;
+const arrOne : number [] = [1, 2, 3];
+const arrTwo : number[] = [4, 5, 6];
+const joinedArr : number[] = arrOne.concat(arrTwo)
 console.log(`Array addition: ${arrOne} + ${arrTwo} is... ${joinedArr}`);
 
 const rectangle = { width: 4, height: 10 };
-const area = rectangle.width * rectangle.heihgt; // an easy typo to make!
+const area = rectangle.width * rectangle.height; // an easy typo to make!
 console.log(
   `Rectangle area: ${rectangle.width} x ${rectangle.height} is... ${area}`
 );
